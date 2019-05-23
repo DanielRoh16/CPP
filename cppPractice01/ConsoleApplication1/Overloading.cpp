2062545f46876e3187c1ae38cc654d98563bf41b
Daniel <daniel.roh16@gmail.com>
2019-05-23 15:00:32 +0900

    add

--- cppPractice01/ConsoleApplication1/Overloading.cpp
+++ cppPractice01/ConsoleApplication1/Overloading.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 using namespace std;
 
@@ -12,24 +13,72 @@ public:
 		m_sum = 0;
 	}
 
-	void AddNum(int a_num)
+	void addNum(int a_num)
 	{
 		m_sum = m_sum + a_num;
 	}
 
+	int GetNum()
+	{
+		return m_sum;
+	}
+
+	void SetNum(int a_num)
+	{
+		m_sum = a_num;
+	}
 	void PrintValue()
 	{
-		cout << "SUM = " << m_sum << endl;
+		cout << "Sum = " << m_sum << endl;
+	}
+};
+
+int main()
+{
+	MySum temp, data;
+	temp.addNum(5);
+	data.SetNum(temp.GetNum() + 7);
+
+	data.PrintValue();
+}
+*/
+
+#include <iostream>
+using namespace std;
+
+class mySum
+{
+private:
+	int m_sum;
+
+public:
+	mySum()
+	{
+		m_sum = 0;
+	}
+	void addNum(int a_num)
+	{
+		m_sum = m_sum + a_num;
+	}
+	void printValue()
+	{
+		cout << "Sum = " << m_sum << endl;
+	}
+	int operator+(int a_num)
+	{
+		return m_sum + a_num;
+	}
+	void operator=(int a_num)
+	{
+		m_sum = a_num;
 	}
 };
 
 int main() 
 {
-	MySum temp;
+	mySum temp, data;
 
-	temp.PrintValue();
-	temp.AddNum(5);
-	temp.PrintValue();
-
-	return 0;
+	temp.addNum(-7);
+	data = temp + (-5);
+	data.printValue();
 }
